41a69a3452fe2660e54cf38adca523a021d94bcb
Josia Scheytt <jscheytt@transline.de>
2021-06-07 17:54:12 +0200

    refactor: install homebrew bundle & omz via ansible

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ This repo can do the following for me (and also for you 😊):
 1. Clone repo to a path of your choice  
 `git clone git@github.com:jscheytt/dotfiles.git`
 1. Enter repo directory and run setup:  
-`./setup.sh .`
+`./setup.sh`
 1. Point iTerm2 to the configuration file `iterm2/com.googlecode.iterm2.plist`. See https://apple.stackexchange.com/a/140624
 1. Update whenever you like by `git pull`
 1. Enjoy 🙂
--- setup.sh
+++ setup.sh
@@ -2,29 +2,26 @@
 set -euo pipefail
 set -x
 
-# Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
-brew bundle install
-# RECOMMENDED: Install color schemes https://github.com/mbadolato/iTerm2-Color-Schemes
-# RECOMMENDED: Copy settings from ./iterm2 to a location of your choice and use it as a starting point for your iTerm2 configuration
-
-# Set up ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-omz update
-# RECOMMENDED: Read the oh_my_zsh documentation and activate plugins that are relevant for you: https://github.com/ohmyzsh/ohmyzsh
-# Powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-echo "ZSH_THEME=powerlevel10k/powerlevel10k" >> ~/.zshrc
-
-# Install gems
-bundle install --system
-
-# Install pip packages
-pip install -r requirements.txt
+which brew || {
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+which pyenv || {
+  brew install pyenv
+}
+which pipenv || {
+  brew install pipenv
+}
+pipenv sync
+pipenv run ansible-galaxy install -r requirements.yml
+pipenv run ansible-playbook setup.yml -K
 
 # Create symlinks
-./create_symlinks.sh
+# TODO: Make idempotent
+# ./create_symlinks.sh
 
 # Set up vim
-./setup_vim.sh
+# TODO: Make idempotent
+# ./setup_vim.sh
+
+# RECOMMENDED: Install color schemes https://github.com/mbadolato/iTerm2-Color-Schemes
+# RECOMMENDED: Copy settings from ./iterm2 to a location of your choice and use it as a starting point for your iTerm2 configuration
