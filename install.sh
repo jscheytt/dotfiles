775c5a790e03d40e488356ef9b7357760a71c978
Josia Scheytt <josia.scheytt@mimacom.com>
2021-10-01 12:03:02 +0200

    build(install): rewrite exec checks via command -v

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 # set -x # DEBUG
 
-which brew > /dev/null || { /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; }
-which pyenv > /dev/null|| { brew install pyenv; }
-which pipenv > /dev/null|| { brew install pipenv; }
+command -v brew > /dev/null || { /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; }
+command -v pyenv > /dev/null|| { brew install pyenv; }
+command -v pipenv > /dev/null|| { brew install pipenv; }
 pyenv install "$(cat .python-version)" --skip-existing
 pipenv sync
 pipenv run ansible-galaxy install -r requirements.yml
