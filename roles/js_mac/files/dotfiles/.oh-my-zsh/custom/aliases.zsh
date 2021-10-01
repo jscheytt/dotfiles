33a9cca84241d61366ddaf708f1f8a4b8c6de04b
Josia Scheytt <josia.scheytt@mimacom.com>
2021-10-01 12:03:49 +0200

    fix(git): set default origin on uninitialized branches

--- roles/js_mac/files/dotfiles/.oh-my-zsh/custom/aliases.zsh
+++ roles/js_mac/files/dotfiles/.oh-my-zsh/custom/aliases.zsh
@@ -4,6 +4,7 @@ alias bfg='java -jar ~/Applications/bfg-1.13.0.jar'
 alias bym='bundle install && yarn install && rails db:migrate && rails data:migrate'
 alias extstat="find . -type f -name '*.*' -not -iwholename '*.svn*' -not -iwholename '*.git*' -print | sed 's/.*\.//' | sort | uniq -c | sort -r"
 alias fp="fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'"
+alias gpi='git push -u origin HEAD'
 alias j2y='ruby -ryaml -rjson -e "puts YAML.dump(JSON.parse(STDIN.read))"'
 alias k9l="k9s info | grep Logs | awk '{ print \$2 }' | sed -e $'s#\033\[[;0-9]*m##g' | xargs vim"
 alias kdebug='kubectl run tmp-shell --rm -i --tty --image nicolaka/netshoot -- /bin/bash'
