b878a86e924fb3b52b4d11c99b064c246191da53
Josia Scheytt <josia.scheytt@mimacom.com>
2021-10-04 10:19:15 +0200

    style: insert spaces

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 # set -x # DEBUG
 
 command -v brew > /dev/null || { /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; }
-command -v pyenv > /dev/null|| { brew install pyenv; }
-command -v pipenv > /dev/null|| { brew install pipenv; }
+command -v pyenv > /dev/null || { brew install pyenv; }
+command -v pipenv > /dev/null || { brew install pipenv; }
 pyenv install "$(cat .python-version)" --skip-existing
 pipenv sync
 pipenv run ansible-galaxy install -r requirements.yml
