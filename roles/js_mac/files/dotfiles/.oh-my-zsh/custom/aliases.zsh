fc478ef46b8b2e0f1503878066239d492265b0cc
Josia Scheytt <josia.scheytt@mimacom.com>
2022-02-24 16:01:06 +0100

    feat(zsh): add functions alias

--- roles/js_mac/files/dotfiles/.oh-my-zsh/custom/aliases.zsh
+++ roles/js_mac/files/dotfiles/.oh-my-zsh/custom/aliases.zsh
@@ -4,6 +4,7 @@ alias bfg='java -jar ~/Applications/bfg-1.13.0.jar'
 alias bym='bundle install && yarn install && rails db:migrate && rails data:migrate'
 alias extstat="find . -type f -name '*.*' -not -iwholename '*.svn*' -not -iwholename '*.git*' -print | sed 's/.*\.//' | sort | uniq -c | sort -r"
 alias fp="fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'"
+alias functions='${=EDITOR} $ZSH_CUSTOM/functions.sh'
 alias gpi='git push -u origin HEAD'
 alias k9cf='nvim "/Users/josia.scheytt/Library/Application Support/k9s/config.yml"'
 alias k9l="k9s info | grep Logs | awk '{ print \$2 }' | sed -e $'s#\033\[[;0-9]*m##g' | xargs ${=EDITOR}"
