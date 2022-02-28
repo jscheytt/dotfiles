6ecafdb1d035ec62cac13ee45046a91d2015c40a
Josia Scheytt <josia.scheytt@mimacom.com>
2022-02-28 11:46:43 +0100

    feat(alias): remove obsolete functions

--- files/dotfiles/.oh-my-zsh/custom/aliases.zsh
+++ files/dotfiles/.oh-my-zsh/custom/aliases.zsh
@@ -5,7 +5,6 @@ alias date-iso='date -u +"%FT%TZ"'
 alias extstat="find . -type f -name '*.*' -not -iwholename '*.svn*' -not -iwholename '*.git*' -print | sed 's/.*\.//' | sort | uniq -c | sort -r"
 alias fp="fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'"
 alias functions='${=EDITOR} $ZSH_CUSTOM/functions.sh'
-alias git-prune-remote-deleted-branches='git remotely-deleted-branches | xargs git branch -d'
 alias gpi='git push -u origin HEAD'
 alias k9cf='nvim "/Users/josia.scheytt/Library/Application Support/k9s/config.yml"'
 alias k9l="k9s info | grep Logs | awk '{ print \$2 }' | sed -e $'s#\033\[[;0-9]*m##g' | xargs ${=EDITOR}"
@@ -15,11 +14,8 @@ alias myip='curl -s https://api.ipify.org | xargs'
 alias nd='nvim -d'
 alias nv='nvim'
 alias nvo="nvim -s <(printf 'B3jo2jojJo')"
-alias php53='docker run -it --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp php:5.3.29-cli'
 alias ssh-config='${=EDITOR} ~/.ssh/config'
 alias tfgraph='terraform graph -draw-cycles | dot -Tsvg > graph.svg'
-alias tfmt='tf fmt'
-alias y2j='ruby -ryaml -rjson -e "puts JSON.pretty_generate(YAML.load(STDIN.read))"'
 alias weekly-reports='bat "$HOME"/Documents/mbio/reports/*'
 
 source ${0:a:h}/functions.sh
