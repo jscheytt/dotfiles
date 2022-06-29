2d89e536f99516863a8cab1233e7022f6ca1a146
Josia Scheytt <josia.scheytt@mimacom.com>
2022-06-29 16:26:32 +0200

    feat(gitpod): add install for gitpod workspace pt. 5

--- install.sh
+++ install.sh
@@ -4,18 +4,22 @@ IFS=$'\n\t'
 # set -x # DEBUG
 
 ### This is the installation script for Gitpod
+### It has a hard timeout of 2mins, so it's a very stripped down version of the
+# normal installation.
 
-
-# brew is assumed
-command -v pipenv > /dev/null || { brew install pipenv; }
-
-# Enter and leave this directory
+# Enter and leave this script's directory
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Make sure to go back to previous directory
 trap popd EXIT
 pushd "$SCRIPT_DIR"
-pipenv sync
-pipenv run ansible-galaxy install -r requirements.yml
-pipenv run ansible-playbook gitpod.yml \
+
+# Install Ansible & Requirements
+python3 -m pip install --user ansible
+ansible-galaxy install -r requirements-gitpod.yml
+
+# Ignore desired Python version
+rm -f .python-version
+# Install everything via Ansible
+ansible-playbook gitpod.yml \
   --inventory inventory \
   -vv
