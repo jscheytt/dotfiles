0a06661e35c745cbf0cd0e38501e9daf15fc0d91
Josia Scheytt <josia.scheytt@mimacom.com>
2023-06-16 08:52:21 +0200

    feat(alias): remove unused aliases

--- files/dotfiles/.oh-my-zsh/custom/aliases.zsh
+++ files/dotfiles/.oh-my-zsh/custom/aliases.zsh
@@ -4,11 +4,9 @@ alias date-iso='date -u +"%FT%TZ"'
 alias extstat="find . -type f -name '*.*' -not -iwholename '*.svn*' -not -iwholename '*.git*' | sed 's/.*\.//' | sort | uniq -c | sort -r"
 alias fp="fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'"
 alias functions='${=EDITOR} $ZSH_CUSTOM/functions.sh'
-alias gpi='git push -u origin HEAD'
 alias k9cf='nvim "/Users/josia.scheytt/Library/Application Support/k9s/config.yml"'
 alias k9l="k9s info | grep Logs | awk '{ print \$2 }' | sed -e $'s#\033\[[;0-9]*m##g' | xargs ${=EDITOR}"
 alias ll='exa -la'
-alias mdl='doru pipelinecomponents/markdownlint'
 alias mkdocs='docker run --rm -it -v ${PWD}:/docs -p 8000:8000 squidfunk/mkdocs-material'
 alias myip='curl -s https://api.ipify.org | xargs'
 alias nd='nvim -d'
@@ -20,7 +18,6 @@ alias tfp='terraform plan'
 alias tfa='terraform apply'
 alias tfaa='terraform apply -auto-approve'
 alias tfgraph='terraform graph -draw-cycles | dot -Tsvg > graph.svg'
-alias weekly-reports='bat "$HOME"/Documents/crontab-reports/*'
 
 # Global
 alias -g k='kubectl'
