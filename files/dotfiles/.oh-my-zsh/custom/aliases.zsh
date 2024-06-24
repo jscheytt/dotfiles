905d98b3eb719423607c4aa5bd71c36b390943e9
Josia Scheytt <josia.scheytt@mimacom.com>
2024-06-24 08:22:45 +0200

    fix(brew): fix some brew doctor warnings

--- files/dotfiles/.oh-my-zsh/custom/aliases.zsh
+++ files/dotfiles/.oh-my-zsh/custom/aliases.zsh
@@ -9,7 +9,7 @@ alias functions='${=EDITOR} $ZSH_CUSTOM/functions.sh'
 alias httpyac="docker run -it -v ${PWD}:/data ghcr.io/anweber/httpyac:latest"
 alias k9cf='nvim "/Users/josia.scheytt/Library/Application Support/k9s/config.yml"'
 alias k9l="k9s info | grep Logs | awk '{ print \$2 }' | sed -e $'s#\033\[[;0-9]*m##g' | xargs ${=EDITOR}"
-alias ll='exa -la'
+alias ll='eza -la --icons'
 alias mkdocs='docker run --rm -it -v ${PWD}:/docs -p 8000:8000 squidfunk/mkdocs-material'
 alias myip='curl -s https://api.ipify.org | xargs'
 alias nd='nvim -d'
