e1d76e3604d11cab26d2af6e4d030bbbe2e90cb4
Josia Scheytt <josia.scheytt@mimacom.com>
2024-06-28 09:10:04 +0200

    fix: fix build target

--- helpers/main.mk
+++ helpers/main.mk
@@ -1,8 +1,8 @@
 HELPTEXT_HEADING := Main Targets:
 
 .PHONY: build
-playbook = main.yaml
-build: install $(vault_password_file) $(become_password_file) ## Run Ansible playbook. Accepted parameters: playbook
+playbook = main.yml
+build: $(vault_password_file) $(become_password_file) ## Run Ansible playbook. Accepted parameters: playbook
 	$(pipenv_command) run ansible-playbook $(playbook) \
 		--extra-vars="ansible_python_interpreter=$$(which python)" \
 		--inventory inventory \
