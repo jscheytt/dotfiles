e5c501b9b334ff706be462d19d4f48c805a7b811
Josia Scheytt <josia.scheytt@mimacom.com>
2024-08-06 15:15:58 +0200

    docs(bootstrap): add inline comments

--- helpers/bootstrap.mk
+++ helpers/bootstrap.mk
@@ -3,11 +3,17 @@ HELPTEXT_HEADING := Bootstrapping Targets:
 .PHONY: install
 pipenv_command = python3 -m pipenv
 install: git-hooks ## Install dependencies.
+	@# Install homebrew.
 	command -v brew > /dev/null || { /bin/bash -c "$$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; }
+	@# Install pyenv.
 	command -v pyenv > /dev/null || { brew install pyenv; }
+	@# Install target Python version.
 	pyenv install "$$(cat .python-version)" --skip-existing
+	@# Install pipenv.
 	pip3 install --break-system-packages --upgrade --user pipenv
+	@# Install Python dependencies from Pipfile.
 	$(pipenv_command) sync --dev
+	@# Install Ansible collections and modules.
 	$(pipenv_command) run ansible-galaxy install \
 		--role-file requirements.yml \
 		--force
