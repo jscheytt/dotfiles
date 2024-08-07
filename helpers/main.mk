739770ff589ab4e46d93489cf24613cce9be086b
Josia Scheytt <josia.scheytt@mimacom.com>
2024-08-07 17:16:47 +0200

    refactor: move vault password file targets to main helper

--- helpers/main.mk
+++ helpers/main.mk
@@ -9,6 +9,15 @@ build: $(vault_password_file) $(become_password_file) ## Run Ansible playbook. A
 		--vault-password-file $(vault_password_file) \
 		-vv
 
+$(vault_password_file):
+	@echo 'Generating random vault password ...'
+	@LC_ALL=C tr -dc A-Za-z0-9 < /dev/urandom | head -c 32 > $@
+
+$(become_password_file): $(vault_password_file)
+	@read -sp 'Enter your admin password (so Ansible can execute sudo commands): ' admin_password; \
+		echo "ansible_sudo_pass: $$admin_password" \
+		| $(pipenv_command) run ansible-vault encrypt --vault-password-file $(vault_password_file) --output $@
+
 .PHONY: update
 update: ## Update pipenv dependencies.
 	$(pipenv_command) update --dev
